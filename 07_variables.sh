501b6132abcd445b812c2d9c6057474df2a1b24e
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 16:22:44 +0530

    TEST

--- 07_variables.sh
+++ 07_variables.sh
@@ -8,6 +8,8 @@ NUMBER=25
 #-ne -> not equal to
 if [ $NUMBER -gt 20 ]; then
     echo " $NUMBER is greater than 20"
-elif 
+elif [ $NUMBER -eq 25 ]; then
+    echo " $NUMBER is equal to 25"
+else
     echo " $NUMBER is less than 20"
 fi
