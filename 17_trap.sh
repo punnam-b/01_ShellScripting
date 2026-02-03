03b9183029240b67cf4d0f7aa33bb9dd3266e710
awsdevopsonline <punnam.elite@gmail.com>
2026-02-03 10:04:22 +0530

    TEST

--- 17_trap.sh
+++ 17_trap.sh
@@ -7,6 +7,6 @@ trap 'echo "An error occurred. Exiting... $BASH_COMMAND"' ERR
 
 echo "Hello World"
 echo "This is to demonstrate trap command"
-echooo 'echo "An error occurred. Exiting..."
+echooo " message of An error occurred. Exiting..."
 echo "This line will execute"
 
